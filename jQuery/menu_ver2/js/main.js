67eebb4a6053c044e3d703251ecc20400dadbc4a
*bibisk* <*beata.g.sukiennik@gmail.com*>
2021-03-14 15:55:48 +0000

    wersja druga warsztatów

---
+++ jQuery/menu_ver2/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('.hamburger').click(function() {
+        if ($('.main-menu').hasClass('open')) {
+            $('.main-menu').slideUp(1000);
+            $('.main-menu').removeClass('open')
+        } else {
+            $('.main-menu').slideDown(1000);
+            $('.main-menu').addClass('open');
+        }
+
+    });
+
+    $('.main-menu-item a').click(function(e) {
+        // preventDefault, by się nie przeładowywało
+        e.preventDefault();
+
+        // efekt wygaszania contentu i odesłanie do strony:
+        $('body').fadeOut(2000, function() {
+            // poniższy zapis zwraca wartość atrybutu klikniętego odnośnika (obiektu)
+            window.location.href = $(e.target).attr('href');
+        });
+
+
+    });
+});
