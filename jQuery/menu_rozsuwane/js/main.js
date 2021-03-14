a3446b6bc1042f1ffee5c0c1c7e06f954f81233b
*bibisk* <*beata.g.sukiennik@gmail.com*>
2021-03-14 15:50:30 +0000

    wersja druga

--- jQuery/menu_rozsuwane/js/main.js
+++ jQuery/menu_rozsuwane/js/main.js
@@ -1,6 +1,33 @@
+// $(document).ready(function() {
+//     $('.hamburger').click(function() {
+//         $('.main-menu').toggleClass('open');
+//     });
+
+//     $('.main-menu-item a').click(function(e) {
+//         // preventDefault, by się nie przeładowywało
+//         e.preventDefault();
+
+//         // efekt wygaszania contentu i odesłanie do strony:
+//         $('body').fadeOut(2000, function() {
+//             // poniższy zapis zwraca wartość atrybutu klikniętego odnośnika (obiektu)
+//             window.location.href = $(e.target).attr('href');
+//         });
+
+
+//     });
+// });
+
+
 $(document).ready(function() {
     $('.hamburger').click(function() {
-        $('.main-menu').toggleClass('open');
+        if ($('.main-menu').hasClass('open')) {
+            $('.main-menu').slideUp(1000);
+            $('.main-menu').removeClass('open')
+        } else {
+            $('.main-menu').slideDown(1000);
+            $('.main-menu').addClass('open');
+        }
+
     });
 
     $('.main-menu-item a').click(function(e) {
